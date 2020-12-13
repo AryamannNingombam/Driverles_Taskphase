1d680e3bfdba6673b6644caec090f0fd304e7dbd
Aryamann Ningombam <aryamannsingh9@gmail.com>
2020-12-13 21:34:36 +0530

    Finished all

--- listPalindrome.py
+++ listPalindrome.py
@@ -1,33 +1,26 @@
 # Code to see if a given list is a palindrome or not
 from math import ceil,floor
 def printPattern(arr):
-    pointer = 0
-
+    arr = ''.join(arr)
+    # a b c d e
+    allElements = []
     length = len(arr)
-    allPatterns = []
-
-    while (pointer <= ceil(length/2)):
-        temp = arr[:pointer+1]
-        stringToPrint=''
-        if (len(temp)<2):
-            stringToPrint = ''.join(temp)
-        else:
-            stringToPrint = ''.join(temp) + ''.join(temp[::-1][1:])
-        allPatterns.append(stringToPrint)
-        
-        pointer+=1
-    secondLength = len(allPatterns[-1])
-    spaces = secondLength//2
-    for i in allPatterns:
-        print(' '*spaces + i)
+    for pointer in range(length):
+        stringToPrint = arr[:pointer+1] + arr[:pointer][::-1]
+        allElements.append(stringToPrint)
+    if (length<4):
+        spaces = ceil(length/2)
+    else:
+        spaces = ceil(length/2) + 2
+    for i in allElements:
+        print(' '*spaces  + i)
         spaces-=1
 
+    spaces +=2
 
-    spaces = 1
-    for i in allPatterns[::-1][1:]:
+    for i in allElements[::-1][1:]:
         print(' '*spaces + i)
         spaces+=1
-
 
 
 
