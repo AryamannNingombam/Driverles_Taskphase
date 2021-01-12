634c0de9f3a01c3f9e53e7f0cc28b68ecaf05a49
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-01-12 23:00:06 +0530

    Added template

---
+++ ROS/firstTask/src/number-sum/scripts/input.py
@@ -0,0 +1,9 @@
+import rospy
+
+
+def runInput():
+    pass
+
+
+if __name__ == '__main__':
+    runInput()
---
+++ ROS/firstTask/src/number-sum/scripts/output.py
@@ -0,0 +1,9 @@
+import rospy
+
+def runOutput():
+    pass
+
+
+
+if __name__ == '__main__':
+    runOutput()
