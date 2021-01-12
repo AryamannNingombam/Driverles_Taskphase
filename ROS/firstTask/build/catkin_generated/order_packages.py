0ba8e808476bb63938c34b6ee8a942477c8bd906
AryamannUbuntu <aryamannsingh9@gmail.com>
2021-01-12 22:33:07 +0530

    Made first ROS

---
+++ ROS/firstTask/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ary_amann/Desktop/formulaManipal/ROS/firstTask/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
