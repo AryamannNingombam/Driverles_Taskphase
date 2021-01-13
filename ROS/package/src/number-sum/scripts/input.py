b7a4e44cb2b8be2c83a511c081f8c385d5657346
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-01-13 12:21:36 +0530

    Added code

--- ROS/package/src/number-sum/scripts/input.py
+++ ROS/package/src/number-sum/scripts/input.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 import rospy
+from std_msgs.msg import Float64MultiArray
 
 
 def runInput():
-    pass
+    pub = rospy.Publisher("getSum",Float64MultiArray,queue_size=10)
     rospy.init_node("Input",anonymous=True)
+    while not rospy.is_shutdown():
+        firstNumber = int(input("Enter the first number : "))
+        secondNumber = int(input("Enter the second number : "))
+        pub.publish([firstNumber,secondNumber])
+
 
 
 if __name__ == '__main__':
-    runInput()
+    try:
+        runInput()
+    except rospy.ROSInterruptException:
+        print("Error!")
--- ROS/package/src/number-sum/scripts/output.py
+++ ROS/package/src/number-sum/scripts/output.py
@@ -2,11 +2,20 @@
 
 
 import rospy
+from std_msgs.msg import Float64MultiArray
+
+def callback(msg):
+    
+    rospy.loginfo(f'The message is {str(msg)}')
+
 
 def runOutput():
-    pass
     rospy.init_node("Output",anonymous=True)
-
+    rospy.Subscriber("getSum", Float64MultiArray, callback)
+    
+    
+    
+    rospy.spin()
 
 if __name__ == '__main__':
     runOutput()
