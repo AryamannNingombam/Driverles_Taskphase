38b3296afb7aeaecee835ec6aed9b65ce33e8d33
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-01-13 10:11:25 +0530

    Made exec

--- ROS/package/src/number-sum/scripts/input.py
+++ ROS/package/src/number-sum/scripts/input.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+
 import rospy
 
 
 def runInput():
     pass
+    rospy.init_node("Input",anonymous=True)
 
 
 if __name__ == '__main__':
--- ROS/package/src/number-sum/scripts/output.py
+++ ROS/package/src/number-sum/scripts/output.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+
+
 import rospy
 
 def runOutput():
     pass
-
+    rospy.init_node("Output",anonymous=True)
 
 
 if __name__ == '__main__':
