5128f43fc4e32872450a001efe0a9de706fd0624
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-01-13 12:42:06 +0530

    Started fresh

--- ROS/package/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ary_amann/Desktop/formulaManipal/ROS/firstTask/src
-# Build directory: /home/ary_amann/Desktop/formulaManipal/ROS/firstTask/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- ROS/package/build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/ary_amann/Desktop/formulaManipal/ROS/firstTask/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
